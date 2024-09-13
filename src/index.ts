3bfe57cfacf254f63c8f7c8d83b2a1ad42709bbf
夏沫花火zzz🌙 <xmcn@1l1.icu>
2024-09-13 18:21:19 +0800

    匹配消息中所有 Speedtest 结果

--- src/index.ts
+++ src/index.ts
@@ -8,13 +8,18 @@ export const Config: Schema<Config> = Schema.object({})
 
 export function apply(ctx: Context) {
   ctx.on('message', (session) => {
-    if (isValidSpeedtestURL(session.content)) {
-      session.send(h('img', { src: `${session.content}.png` }));
+    let uuid = extractSpeedtestUUID(session.content);
+    if (uuid != null) {
+      session.send(h('img', { src: `https://www.speedtest.net/result/c/${uuid}.png` }));
     }
   })
 }
 
-function isValidSpeedtestURL(url) {
-  const pattern = /^https:\/\/www\.speedtest\.net\/result\/c\/[a-f0-9\-]{36}$/i;
-  return pattern.test(url);
+function extractSpeedtestUUID(url) {
+  const pattern = /^https:\/\/www\.speedtest\.net\/result\/c\/([a-f0-9\-]{36})$/i;
+  const match = url.match(pattern);
+  if (match) {
+    return match[1];
+  }
+  return null;
 }
