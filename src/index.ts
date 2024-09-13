1b6f6a3f7c4f381a602097b534d6f469fd7e4f4b
夏沫花火zzz🌙 <xmcn@1l1.icu>
2024-09-13 19:28:44 +0800

    更多匹配

--- src/index.ts
+++ src/index.ts
@@ -8,15 +8,24 @@ export const Config: Schema<Config> = Schema.object({})
 
 export function apply(ctx: Context) {
   ctx.on('message', (session) => {
-    let uuid = extractSpeedtestUUID(session.content);
-    if (uuid != null) {
-      session.send(h('img', { src: `https://www.speedtest.net/result/c/${uuid}.png` }));
+    let key = extractSpeedtestUUID(session.content) ?? extractSpeedtestID(session.content);
+    if (key != null) {
+      session.send(h('img', { src: `https://www.speedtest.net/result/c/${key}.png` }));
     }
   })
 }
 
 function extractSpeedtestUUID(url) {
-  const pattern = /^https:\/\/www\.speedtest\.net\/result\/c\/([a-f0-9\-]{36})$/i;
+  const pattern = /^https:\/\/www\.speedtest\.net\/result\/[^\/]+\/(.+)$/;
+  const match = url.match(pattern);
+  if (match) {
+    return match[1];
+  }
+  return null;
+}
+
+function extractSpeedtestID(url) {
+  const pattern = /^https:\/\/www\.speedtest\.net\/my-result\/[^\/]+\/(.+)$/;
   const match = url.match(pattern);
   if (match) {
     return match[1];
