f4d78f6805550b5488384b614be08525c665a187
夏沫花火zzz🌙 <xmcn@1l1.icu>
2024-09-13 18:11:33 +0800

    Build result

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Context, Schema } from 'koishi'
+import { Context, h, Schema } from 'koishi'
 
 export const name = 'speedtest-og'
 
@@ -7,5 +7,14 @@ export interface Config {}
 export const Config: Schema<Config> = Schema.object({})
 
 export function apply(ctx: Context) {
-  // write your plugin here
+  ctx.on('message', (session) => {
+    if (isValidSpeedtestURL(session.content)) {
+      session.send(h('img', { src: `${session.content}.png` }));
+    }
+  })
+}
+
+function isValidSpeedtestURL(url) {
+  const pattern = /^https:\/\/www\.speedtest\.net\/result\/c\/[a-f0-9\-]{36}$/i;
+  return pattern.test(url);
 }
